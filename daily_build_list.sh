5f67cdf07a7298c8710fc84ec7c77d161ff617ec
louiscklaw <louiscklaw@gmail.com>
2020-05-03 19:56:02 +0800

    update for adding my-new-inventory,

--- daily_build_list.sh
+++ daily_build_list.sh
@@ -8,6 +8,9 @@ bash ./curl_cron.sh louiscklaw%2Fportfolio-preact
 # louiscklaw/esp32-cam-playlist
 bash ./curl_cron.sh louiscklaw%2Fesp32-cam-playlist
 
+# https://travis-ci.com/github/louiscklaw/my-new-inventory
+bash ./curl_cron.sh louiscklaw%2Fmy-new-inventory
+
 echo "done"
 
 exit 0
